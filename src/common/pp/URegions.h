550eb21182dce5544d5ddea13489d897393fbfc0
DaCodeChick <peepvtuber@gmail.com>
2025-04-27 22:46:22 -0700

    Update URegions.h

--- src/common/pp/URegions.h
+++ src/common/pp/URegions.h
@@ -1,13 +1,30 @@
 #pragma once
 
 #ifdef _WIN32
-#include "typedefs.h"
+#include "../typedefs.h"
 
+/**
+ * @brief A class that represents a region in an application.
+ * 
+ */
 class SRegion
 {
 public:
+	/**
+	 * @brief Default constructor that initializes the region to an empty state.
+	 */
 	SRegion();
+
+	/**
+	 * @brief Constructor that initializes the region with a specified rectangle.
+	 * 
+	 * @param inRect The rectangle to initialize the region with.
+	 */
 	SRegion(RECT &inRect);
+
+	/**
+	 * @brief Destructor that cleans up the region.
+	 */
 	virtual ~SRegion();
 
 private:
